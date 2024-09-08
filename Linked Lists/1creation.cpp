b2aaf0f93e9d4038b89feda1193cf36dd25e7502
Manpreet Singh Sidhu <preetsidhu2549@gmail.com>
2024-09-09 00:01:29 +0530

    Creating 1st LinkedList

---
+++ Linked Lists/1creation.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+// Creating a linked list by converting it from an array
+Node* convertToLinkedList(int arr[], int size){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1;i<size;i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+int traversal(Node* head){
+    Node* temp = head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    return 0;
+}
+int lengthOfLinkedList(Node* head){
+    int counter = 0;
+    Node* temp = head;
+    while(temp){
+        temp = temp->next;
+        counter++;
+    }
+    cout<<"\nLength of LinkedList is "<<counter<<endl;
+    return 0;
+}
+
+int main(){
+    int arr[] = {10,20,30,40,50};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    Node* head = convertToLinkedList(arr,size);
+    traversal(head);
+    lengthOfLinkedList(head);
+}
