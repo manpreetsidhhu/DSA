a971a7dfc06c9589d7b599d4991fbea633e32311
Manpreet Singh Sidhu <preetsidhu2549@gmail.com>
2024-09-11 22:03:17 +0530

    Bubble Sort

---
+++ Array/Sorting/BubbleSort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int bubbleSort(int arr[], int size){
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            if (arr[j] > arr[j + 1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }cout<<"\nSorted Array: [";
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }cout<<"]";
+    return 1;
+}
+
+int main(){
+    int n; 
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of array separated by 1space:"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int result = bubbleSort(arr,n);
+}
