7d92e47c45a3ce816f162fc506c6f91cc96ae19e
Manpreet Singh Sidhu <preetsidhu2549@gmail.com>
2024-09-14 17:35:55 +0530

    Insertion at K

--- Linked Lists/2InsertingInLinkedList.cpp
+++ Linked Lists/2InsertingInLinkedList.cpp
@@ -30,6 +30,34 @@ Node* insertAtHead(Node* head, int value){
     Node* temp = new Node(value,head);
     return temp;
 }
+Node* insertAtTail(Node* head, int value){
+    if(head == NULL) return new Node(value);
+    Node* temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    } temp->next = new Node(value);
+    return head;
+}
+Node* insertAtIndex(Node* head, int value, int index){
+    if(head==NULL){
+        if(index == 1){
+            return new Node(value);
+        } else return NULL;
+    }
+    Node* temp = head;
+    Node* prev = NULL;
+    int counter = 0;
+    while(temp != NULL){
+        counter++;
+        if(index == 1) return insertAtHead(head, value);
+        if(index-1 == counter){
+            Node* newNode = new Node(value);
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+        temp = temp->next;
+    } return head;
+}
 int traversal(Node* head){
     Node* temp = head;
     while(temp){
@@ -47,5 +75,15 @@ int main(){
     cout<<"\nEnter Value to be inserted at Head: ";
     int value; cin>>value;
     head = insertAtHead(head, value);
+    traversal(head);
+    cout<<"\nEnter Value to be inserted at Tail: ";
+    int value2; cin>>value2;
+    head = insertAtTail(head, value2);
+    traversal(head);
+    cout<<"\nEnter position where value to be inserted: ";
+    int index; cin>>index;
+    cout<<"Enter Value to be inserted at position "<<index<<" : ";
+    int value3; cin>>value3;
+    head = insertAtIndex(head, value3, index);
     traversal(head);
 }
